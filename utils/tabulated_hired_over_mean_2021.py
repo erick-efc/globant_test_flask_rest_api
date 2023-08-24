47f7f3e309ed2b790d083521ea1c6193a9686e26
-efc <80852618+erick-efc@users.noreply.github.com>
2023-08-24 15:39:05 -0300

    Update tabulated_hired_over_mean_2021.py
    comments removed

--- utils/tabulated_hired_over_mean_2021.py
+++ utils/tabulated_hired_over_mean_2021.py
@@ -4,6 +4,6 @@ from tabulate import tabulate
 url = 'http://localhost:5000/api/sql/hired_over_mean_2021'
 
 response = requests.get(url)
-table = response.json()  # The response should already be the tabulated data as a string
+table = response.json()
 formatted_table = tabulate(table, tablefmt='plain')
 print(formatted_table)
