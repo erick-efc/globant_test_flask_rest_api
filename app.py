0525c74cd986521e67937c4fff858a4460add56c
-efc <80852618+erick-efc@users.noreply.github.com>
2023-08-30 23:51:07 -0300

    omitting company's name from code and files

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, jsonify
 from  utils.functions import connect_now, insert_data_into_table
 from routes.extra_routes import extra_routes_bp
-from routes.globant_required_routes import globant_required_routes_bp
+from routes.required_routes import required_routes_bp
 
 app = Flask(__name__)
 
 # APP CONFIG
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'api_user'
-app.config['MYSQL_PASSWORD'] = 'globant123' # NEVER HARDCODE SENSITIVE INFORMATION, THIS IS JUST FOR DEMONSTRATION PURPOSE
-app.config['MYSQL_DB'] = 'globant_test'
+app.config['MYSQL_PASSWORD'] = 'your_password' # NEVER HARDCODE SENSITIVE INFORMATION, THIS IS JUST FOR DEMONSTRATION PURPOSE
+app.config['MYSQL_DB'] = 'local_api_db'
 app.config['MYSQL_CURSORCLASS'] = 'pymysql.cursors.DictCursor'
 app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  
 app.config['UPLOAD_FOLDER'] = './uploads'
@@ -17,4 +17,4 @@ app.config['UPLOAD_HIST'] = './historical_upload'
 
 # BLUEPRINT REGISTER
 app.register_blueprint(extra_routes_bp)
-app.register_blueprint(globant_required_routes_bp)
+app.register_blueprint(required_routes_bp)
